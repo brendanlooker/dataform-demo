9b925fffaa535dcb83e4ae81c28d68edc552e09f
Brendan Buckley <bbuckley@google.com>
2023-06-16 14:37:55 +0000

    updates

--- includes/constants.js
+++ includes/constants.js
@@ -1,3 +1,7 @@
   // filename is includes/constants.js
   const PROJECT_ID = "brendanlooker";
   module.exports = { PROJECT_ID };
+
+
+  const thelook_ecommerce_public = "bigquery-public-data.thelook_ecommerce";
+  module.exports = { thelook_ecommerce_public };
